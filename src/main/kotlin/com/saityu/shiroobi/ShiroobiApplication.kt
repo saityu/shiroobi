1fc56e0d045017fd4e11819ee51acf875afc0ac1
saityu <s.yusuke.dev@gmail.com>
2021-02-14 11:01:56 +0900

    Add Spring project created by Spring Initializr
    see https://start.spring.io/

---
+++ src/main/kotlin/com/saityu/shiroobi/ShiroobiApplication.kt
@@ -0,0 +1,11 @@
+package com.saityu.shiroobi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShiroobiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ShiroobiApplication>(*args)
+}
---
+++ src/test/kotlin/com/saityu/shiroobi/ShiroobiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.saityu.shiroobi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShiroobiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
